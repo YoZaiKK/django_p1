70231baca5d72409173aef16495f50027ac13a87
Guillermo <memocargo@hotmail.com>
2022-01-12 20:02:46 -0600

    first commit

---
+++ ProyectoWebApp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+def inicio(request):
+    return HttpResponse("Inicio")
+
+def servicios(request):
+    return HttpResponse("Servicios")
+
+def tienda(request):
+    return HttpResponse("Tienda")
+
+def blog(request):
+    return HttpResponse("Blog")
+
+def contacto(request):
+    return HttpResponse("Contacto")
+
+
+
+
