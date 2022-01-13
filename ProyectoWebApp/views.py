83d39eab589e9e58c4eeb868d4fa3eeb57f2dd32
Guillermo <memocargo@hotmail.com>
2022-01-12 20:44:43 -0600

    views creadas y agregadas, aun no refinadas

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -4,20 +4,25 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("Inicio")
+
+    return render(request, 'ProyectoWebApp/inicio.html')
+
 
 def servicios(request):
-    return HttpResponse("Servicios")
+
+    return render(request, 'ProyectoWebApp/servicios.html')
+
 
 def tienda(request):
-    return HttpResponse("Tienda")
+
+    return render(request, 'ProyectoWebApp/tienda.html')
+
 
 def blog(request):
-    return HttpResponse("Blog")
+
+    return render(request, 'ProyectoWebApp/blog.html')
+
 
 def contacto(request):
-    return HttpResponse("Contacto")
 
-
-
-
+    return render(request, 'ProyectoWebApp/contacto.html')
