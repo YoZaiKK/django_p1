c75c0b45ca3fc6b75c13cf5694f9e286b704da83
Guillermo <memocargo@hotmail.com>
2022-06-15 17:34:11 -0500

    parte correspondiente a servicios completada

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -6,17 +6,13 @@ def inicio(request):
 
     return render(request, 'ProyectoWebApp/inicio.html')
 
-
-
 def tienda(request):
 
     return render(request, 'ProyectoWebApp/tienda.html')
 
-
 def blog(request):
 
     return render(request, 'ProyectoWebApp/blog.html')
-
 
 def contacto(request):
 
--- servicios/urls.py
+++ servicios/urls.py
@@ -7,5 +7,4 @@ from django.conf.urls.static import static
 urlpatterns = [ 
     path('', views.servicios, name='Servicios'),
 ] 
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+ 
