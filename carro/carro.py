968342060e1057792cfa22a94997eecb3f971f97
Guillermo <memocargo@hotmail.com>
2022-07-15 15:00:01 -0500

    funciones para el funcionamiento del carro creadas y parecen funcionar bien

--- carro/carro.py
+++ carro/carro.py
@@ -1,22 +1,52 @@
 class Carro:
     # detectar la request
     # construir la session
-    # construir el carro 
+    # construir el carro
     def __init__(self, request):
-        self.request = request #aqui almacenamos la peticion
-        self.session = request.session #tenemos iniciada la session
-        carro = self.session.get('carro') #aqui identifica el string con el carro
-        if not carro:   # Lo creamos, vea
-            carro = self.session['carro'] = {} # inicializamos el carro
-        else:   # Ps no lo creamos, vea
+        self.request = request  #aqui almacenamos la peticion
+        self.session = request.session  #tenemos iniciada la session
+        carro = self.session.get(
+            'carro')  #aqui identifica el string con el carro
+        if not carro:  # Lo creamos, vea
+            carro = self.session['carro'] = {}  # inicializamos el carro
+        else:  # Ps no lo creamos, vea
             self.carro = carro
-    def agregar(self, producto): #funcion que agregue los productos al carro
-        if(str(producto.id) not in self.carro.key()):
+
+    def agregar(self, producto):  #funcion que agregue los productos al carro
+        if (str(producto.id) not in self.carro.key()):
             self.carro[producto.id] = {
-                "producto_id": producto.id,
-                "nombre": producto.nombre,
-                "precio": str(producto.precio),
-                "cantidad": 1,
-                "imagen": producto.imagen.url,
-                
+                'producto_id': producto.id,
+                'nombre': producto.nombre,
+                'precio': str(producto.precio),
+                'cantidad': 1,
+                'imagen': producto.imagen.url,
             }
+        else:
+            for key, value in self.carro.items():
+                if key == str(producto.id):
+                    value['cantidad'] = value['cantidad'] + 1
+                    break
+        self.guardar_carro()
+
+    def guardar_carro(self):
+        self.session['carro'] = self.carro
+        self.session.modified = True
+
+    def eliminar(self, producto):
+        producto.id = str(producto.id)
+        if producto.id in self.carro.key():
+            del self.carro[producto.id]
+            self.guardar_carro()
+
+    def restar_producto(self, producto):
+        for key, value in self.carro.items():
+            if key == str(producto.id):
+                value['cantidad'] = value['cantidad'] - 1
+                if value['cantidad'] < 1:
+                    self.eliminar(producto)
+                break
+        self.guardar_carro()
+        
+    def vaciar_carro(self, producto):
+        self.session['carro'] = {}
+        self.session.modified = True
